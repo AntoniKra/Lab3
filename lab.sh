b57791aa4ca662bae6e3a074d8400f3b9b2af154
Amtoni <dsw52717@student.dsw.edu.pl>
2024-04-28 18:03:08 +0200

    Final step

---
+++ lab.sh
@@ -0,0 +1,31 @@
+git init
+git clone [url]
+git add [nazwa_pliku]
+git add .
+git commit -m "[wiadomość_commita]"
+git status
+git push
+git pull
+git branch
+git checkout [nazwa_gałęzi]
+git merge [nazwa_gałęzi]
+git log
+git reset [nazwa_pliku]
+git remote -v
+git stash
+git remote add [nazwa] [url]
+git remote remove [nazwa]
+git fetch
+git checkout -b [nazwa_gałęzi]
+git branch -d [nazwa_gałęzi]
+git branch -m [stara_nazwa] [nowa_nazwa]
+git commit --amend
+git pull --rebase
+git diff
+git tag [nazwa_tagu]
+git push --tags
+git revert [numer_commita]
+git cherry-pick [numer_commita]
+git blame [nazwa_pliku]
+git clean -n
+git config --global [opcja]
